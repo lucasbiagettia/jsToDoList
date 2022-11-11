d0fe451937daf72787663c2c9564354c3089fee8
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2022-11-11 12:18:02 -0300

    Arrow function en JS

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector('[data-form-btn]');
-btn.addEventListener('click', function () {
+
+const createTask = (event) => {
+    event.preventDefault();
     const input = document.querySelector ('[data-form-input');
     console.log(input.value);
-});
+}
+
+btn.addEventListener('click', createTask);
