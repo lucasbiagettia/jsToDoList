6c47ae2a7af196e955e0604830819a5e23030fd3
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2022-11-11 11:58:51 -0300

    agregado js

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('[data-form-btn]');
+btn.addEventListener('click', function () {
+    const input = document.querySelector ('[data-form-input');
+    console.log(input.value);
+});
