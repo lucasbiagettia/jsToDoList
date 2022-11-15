357d0ab24c83cc8e74269c7a44e948c106ce9b95
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-14 22:32:58 -0300

    add cards

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const createTask = (event) => {
     event.preventDefault();
     const input = document.querySelector ('[data-form-input');
     const value = input.value;
-    const task = document.querySelector('[data-task]');
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
     input.value = '';
     const content = `<div>
     <i class="far fa-check-square icon"></i>
@@ -13,6 +15,7 @@ const createTask = (event) => {
     </div>
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
     task.innerHTML = content;
+    list.appendChild(task);
     console.log(value);
 }
 
