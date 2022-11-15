c8990332369fb0f2a5c24fc6c4b66e8dc4f319ec
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-14 22:42:23 -0300

    dynamic

--- script.js
+++ script.js
@@ -9,14 +9,27 @@ const createTask = (event) => {
     const task = document.createElement("li");
     task.classList.add("card");
     input.value = '';
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete);
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
     task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
     console.log(value);
-}
+};
 
 btn.addEventListener('click', createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    return i;
+
+}
