22de1463a6e62fc5ba14742ca0452d7761b24872
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-24 23:45:35 -0300

    finished storage

--- components/addTask.js
+++ components/addTask.js
@@ -2,46 +2,47 @@ import checkComplete from './checkComplete.js';
 import deleteTask from './deleteTask.js';
 
 export const addTask = (event) => {
+    event.preventDefault();
+    const input = document.querySelector ('[data-form-input');
+    const calendar = document.querySelector( '[data-form-date]');
+    const value = input.value;
+    const date = calendar.value;
+    const dateFormat = moment(date).format("DD/MM/YYYY");
     const list = document.querySelector('[data-list]');
-    const task = createTask(event);
+    input.value = '';
+    calendar.value = '';
+    const taskObject = {
+        value,
+        dateFormat,
+    };
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    taskList.push({value, dateFormat});
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    const task = createTask(taskObject);
     list.appendChild(task);
 
 }
 
 
 
-const createTask = (event) => {
-    event.preventDefault();
-    const input = document.querySelector ('[data-form-input');
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    const calendar = document.querySelector( '[data-form-date]');
-    const value = input.value;
-    const date = calendar.value;
-    const dateFormat = moment(date).format("DD/MM/YYYY");
-    const list = document.querySelector('[data-list]');
+export const createTask = ({value, dateFormat}) => {
     const task = document.createElement('li');
     task.classList.add('card');
-    input.value = '';
+    
     const taskContent = document.createElement('div');
-    const taskObject = {
-        value,
-        dateFormat,
-    }
-
-    taskList.push(taskObject);
-    localStorage.setItem('tasks', JSON.stringify(taskList));
-    taskContent.appendChild(checkComplete());
+    
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
+    
     const dateElement = document.createElement("span");
     dateElement.innerHTML = dateFormat;
-    
+
     task.appendChild(taskContent);
     task.appendChild(dateElement);
     task.appendChild(deleteTask());
     return task;
-
 };
 
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import {addTask} from './components/addTask.js';
+import {readTasks} from './components/readTasks.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
 btn.addEventListener('click', addTask);
+
+readTasks();
 
 
 
