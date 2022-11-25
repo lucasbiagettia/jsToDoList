b9af3381e6aece8a5a76f30bd6818b37708aee6c
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-24 22:30:14 -0300

    mejoras date

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ import checkComplete from './components/checkComplete.js';
 import deleteTask from './components/deleteTask.js';
 
 
-    const btn = document.querySelector('[data-form-btn]');
+const btn = document.querySelector('[data-form-btn]');
 
+    const addTask = (event) => {
+        const list = document.querySelector('[data-list]');
+        const task = createTask(event);
+        list.appendChild(task);
+
+    }
 
     const createTask = (event) => {
         event.preventDefault();
@@ -11,7 +17,7 @@ import deleteTask from './components/deleteTask.js';
         const calendar = document.querySelector( '[data-form-date]');
         const value = input.value;
         const date = calendar.value;
-        // moment (date).format("DD/MM/YYYY");
+        const dateFormat = moment(date).format("DD/MM/YYYY");
         const list = document.querySelector('[data-list]');
         const task = document.createElement('li');
         task.classList.add('card');
@@ -22,12 +28,17 @@ import deleteTask from './components/deleteTask.js';
         titleTask.classList.add('task');
         titleTask.innerText = value;
         taskContent.appendChild(titleTask);
+        const dateElement = document.createElement("span");
+        dateElement.innerHTML = dateFormat;
+        
         task.appendChild(taskContent);
+        task.appendChild(dateElement);
         task.appendChild(deleteTask());
-        list.appendChild(task);
+        return task;
+  
     };
 
-    btn.addEventListener('click', createTask);
+    btn.addEventListener('click', addTask);
 
 
 
