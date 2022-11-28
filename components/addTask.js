b0319da8af5f893cc793c7ca4107529710dcf9e9
Lucas Biagetti <lucasbiagetti@gmail.com>
2022-11-28 16:40:58 -0300

    Update addTask.js

--- components/addTask.js
+++ components/addTask.js
@@ -58,7 +58,6 @@ export const createTask = ({value, dateFormat, complete, id}) => {
     dateElement.innerHTML = dateFormat;
   
     task.appendChild(taskContent);
-    task.appendChild(dateElement);
     task.appendChild(deleteIcon(id));
     return task;
 };
